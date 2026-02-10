f51c1f885d827e0fcd32f8affc3662d53ad21cc5
Mrutyunjay Giri <mrutyunjaygiri470@gmail.com>
2026-02-10 22:51:01 +0530

    Delete day2 directory

--- day2/Strings_Java.java
+++
@@ -1,9 +0,0 @@
-
-import java.lang.*;
-
-public class Strings_Java {
-    public static void main(String[] args) {
-        String name="Mrutyunjay";
-        System.out.println("name: " + name);
-    }
-}
